58b5cec51d12a61a462efbc6d7d356743bc572fa
ishrath1-s <ishrath.md6@gmail.com>
2025-03-15 20:50:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thanks for reaching out! I\'ll get back to you soon.');
+    // You can add more functionality here, like sending the form data to a server.
+});
