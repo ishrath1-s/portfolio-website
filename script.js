932081ebed3f842bb36ab5ba438cca412421005b
ishrath1-s <ishrath.md6@gmail.com>
2025-03-15 20:54:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thanks for reaching out! I\'ll get back to you soon.');
-    // You can add more functionality here, like sending the form data to a server.
 });
